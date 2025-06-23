a7b8146bf96babaa0e9a0e515dd0d5a5a0a3e523
zhenghchen <zhenghchen2024@gmail.com>
2025-06-23 11:00:46 -0700

    Timer with Thread.sleep

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,18 @@
 import java.util.Scanner;
-import java.util.Random;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
+        System.out.print("Enter a time (s): ");
+        int duration = scanner.nextInt();
+        while (duration != 0) {
+            System.out.println(duration);
+            duration--;
+            Thread.sleep(1000);
+        }
 
-        int randInt = random.nextInt(1, 1001);
+        System.out.println("TIMER IS OVER");
 
-        System.out.println("Number Guessing Game");
-        System.out.println("Guess a number between 1-1000");
-        int guess;
-        int attempts = 0;
-        do {
-            System.out.print("Enter a guess: ");
-            guess = scanner.nextInt();
-            if (guess > randInt) {
-                System.out.println("Lower!");
-            }
-            else if (guess < randInt) {
-                System.out.println("Higher!");
-            }
-            attempts++;
-        } while (guess != randInt);
-
-        System.out.println("You won in " + attempts + " attempts!");
+        scanner.close();
     }
 }
